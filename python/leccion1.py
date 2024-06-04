d535f4ff5664d69bd760d2ea7cae81d86403a961
Marina Rebolloso <marina.rebolloso@gmail.com>
2024-06-03 21:22:14 -0300

    Ejercicios de práctica

---
+++ python/leccion1.py
@@ -0,0 +1,32 @@
+miVariable = 3
+print(miVariable)
+miVariable = "Hello a todos los estudiantes de la tecnicatura"
+print(miVariable)
+miVariable = 3.5 
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(id(x))
+
+#Las literales se escriben x056
+print(id(z))
+print(id(y))
+
+#Tipos de datos
+
+x = 10
+print(x)
+print(type(x))
+x = 14.5
+print(x)
+print(type(x))
+x = "Hola alumnos"
+print(x)
+print(type(x))
+x = True
+print(x)
+print(type(x))
+x = False
+print(x)
+print(type(x))
