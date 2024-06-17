2ac0279d3a48a81b95ebfafb62a21c75bc984dce
Marina Rebolloso <marina.rebolloso@gmail.com>
2024-06-17 19:29:13 -0300

    Agregamos ejercicios de concatenación de cadenas

---
+++ python/leccion2/Ejercicio1.py
@@ -0,0 +1,7 @@
+#Ejercicio 1: plasmar una expresión algorítmica en código
+
+a = float(input("Ingrese el valor de a"))
+b= float(input("Ingrese el valor de b"))
+c = float(input("Ingrese el valor de c"))
+resultado = (a**3 * (b**2 - 2*a*c)) / (2*b)
+print(f"El resultado es: {resultado}")
--- python/leccion2/leccion2.py
+++ python/leccion2/leccion2.py
@@ -1,3 +1,4 @@
+"""
 # Sentencia if/else
 
 condicion = True
@@ -7,3 +8,25 @@ elif condicion == False:
     print("Condición falsa")
 else:
     print("Condición sin especificar")
+"""
+#Conversión de número a texto
+
+#num = int(input("Digite un número"))
+#numTexto = ""
+#if num == 1:
+#    numTexto = "Número uno"
+#elif num == 2:
+#    numTexto = "Número dos"
+#elif num == 3:
+#    numTexto = "Número tres"
+#else:
+#    numTexto = "Has ingresado un número fuera de rango"
+#print(f"El número ingresado es {num} - {numTexto}")
+
+condicion = False
+#if condicion:
+#    print("Condición verdadera")
+#else:
+#    print("Condición falsa")
+
+print("Condición verdadera") if condicion else print("Condición falsa")
