a4c2b941c5deb24b60c0ba4567dcf6d2b5c5cf47
Marina Rebolloso <marina.rebolloso@gmail.com>
2024-06-17 20:42:38 -0300

    Agragado cambios en lección 2 concatenación de cadenas y finalizada clase 7

--- Javascript/Leccion02/02-02-concatenacionDeCadenas.js
+++ Javascript/Leccion02/02-02-concatenacionDeCadenas.js
@@ -13,3 +13,18 @@ console.log(juntos);
 
 nombre += apellido // Concatenación simplificada
 console.log(nombre);
+
+//Ya no se usa var se usa let y const
+let nombre2 = "Pedro ";
+console.log(nombre2);
+
+const apellido2 = "Lepes";
+//apellido2 = "Perez" una constante no puede ser modificada
+console.log(apellido2);
+
+let x, y; //Se pueden crear varias variables en una misma línea
+x = 17, y = 21; // se asignan distintas variables en una misma línea
+let z = x + y; //se asigna valor de la operación
+console.log(x, y, z);
+
+
---
+++ Javascript/Leccion03/03-01-operadoresJavascript.js
@@ -0,0 +1,19 @@
+//Ejercicio para encontrar números pares e impares
+
+let parImpar = 10;
+if (parImpar % 2 == 0){
+console.log("El número es par")
+}
+else {
+    console.log ("El número es impar")
+}
+
+//Ejercicio mayor de edad
+
+let edad = 14, adulto = 18;
+if (edad >= adulto){
+    console.log("Usted es una persona adulta")
+}
+else{
+    console.log("Usted es una persona menor de edad")
+}
