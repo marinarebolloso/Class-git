b96a00580d473fbb826b0eb60593f7c9b202649f
Marina Rebolloso <marina.rebolloso@gmail.com>
2024-06-24 21:51:30 -0300

    Ejercicios clase 9 y 10 finalizados

--- Javascript/Leccion03/03-01-operadoresJavascript.js
+++ Javascript/Leccion03/03-01-operadoresJavascript.js
@@ -28,3 +28,38 @@ if (dentroRango >= valMin && dentroRango <= valMax){
 else{
     console.log("Está fuera del rango")
 }
+
+//Ejercicio: si el padre puede o no asistir al juego del hijo
+let vacaciones = false, diaDescanso = false;
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir al juego")
+}
+else{
+    console.log("Elpadre no puede asistir")
+}
+
+//Operador ternario
+
+let resultado2 = 3 > 2 ? "Verdadero" : "Falso";
+console.log(resultado2)
+let numero = 9;
+resultado2 = numero % 2 == 0 ? "Este es un número par" : "Este es un número impar";
+console.log(resultado2)
+
+// Convertir string a número
+
+let miNumero = "10"; //cadena
+console.log(typeof miNumero);
+let edad2 = Number(miNumero);
+console.log(typeof edad2);
+if(isNaN(edad2 >= 18)){
+    console.log("Puede votar");
+}
+else{
+    console.log("Muy joven para votar");
+}
+
+let resultado3 = edad2 >= 18 ? "Puede votar": "Muy joven para votar";
+console.log(resultado3)
+
+//Función isNaN
--- Javascript/Leccion04/04-01-Ejercicios.js
+++ Javascript/Leccion04/04-01-Ejercicios.js
@@ -1,3 +1,61 @@
+//Ejercicio 1: calculando estación del año
+let mes = 4;
+let estacion; //undefined
+if (mes == 1 || mes == 2 || mes == 12){
+    estacion = "Verano";
+} 
+else if (mes == 3 || mes == 4 || mes == 5){
+    estacion = "Otoño";
+}
+else if (mes == 6 || mes == 7 || mes == 8){
+    estacion = "Invierno";
+}
+else if (mes == 9 || mes == 10 || mes == 11){
+    estacion = "Primavera";
+}
+else{
+    estacion = "Opción incorrecta"
+}
+console.log("El valor corresponde a la estación: "+estacion);
+//Ejercicio 2: hora del día
+let horaDia = 3;
+let mensaje;
+if (horaDia >= 6 && horaDia <= 11){
+    mensaje = "Good morning"
+}
+else if (horaDia >= 12 && horaDia <= 16){
+    mensaje = "Good afternoon"
+}
+else if (horaDia >= 17 && horaDia <= 19){
+    mensaje = "Good evening"
+}
+else if (horaDia >= 20 && horaDia <= 23){
+    mensaje = "Good night"
+}
+else if (horaDia >= 00 && horaDia <= 5){
+    mensaje = "Sweet dreams"
+}
+else{
+    mensaje = "Error"
+}
+console.log(mensaje)
+
+//Estructura switch (misma sintaxis java)
+switch(mes){
+    case 1: case 2: case 12:
+        estacion = "Verano";
+    case 3: case 4: case 5:
+        estacion = "Otoño";
+    case 6: case 7: case 8:
+        estacion = "Invierno";
+    case 9: case 10: case 11:
+        estacion = "Primavera";
+        break;
+    default:
+        estacion = "Valor incorrecto";
+}
+console.log("Bienvenido a la estación: "+estacion);
+
 //Ampliando el uso de var let y const
 
 var nombre = "Ariel";
@@ -8,10 +66,15 @@ function saludar(){
     var nombre = "Natalia";
     console.log(nombre);
 }
-console.log(nombre); //Aquín no lee el dato en la función
+//console.log(nombre); //Aquín no lee el dato en la función
 
 if(true){
     var edad = 34;
     console.log(edad);
 }
 console.log(edad); //En la función anda correctamente, en la estructura if falla
+
+const fechaNacimiento = 2006;
+console.log(fechaNacimiento);
+//fechaNacimiento = 2003;
+console.log(fechaNacimiento); //solo
